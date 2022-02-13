8594d65be6fa94e155c8d97f3bb4287c585d5c40
Philippe Rivest <technophil98@gmail.com>
2022-02-12 23:54:39 -0500

    Add sentiment analysis to action server

--- actions/actions.py
+++ actions/actions.py
@@ -4,34 +4,13 @@
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
 
-
-# This is a simple example for a custom action which utters "Hello World!"
-
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
-
 from typing import Any, Text, Dict, List
 
-from rasa_sdk import Action, Tracker
+from rasa_sdk import Action, Tracker, logger
 from rasa_sdk.executor import CollectingDispatcher
 
 from actions.db import service_comments, db_conn
+from actions.sentiment import sample_analyze_sentiment
 
 
 class ActionAcknowledgeComment(Action):
@@ -46,9 +25,11 @@ class ActionAcknowledgeComment(Action):
         if len(tracker.latest_message["entities"]) > 0:
             message = tracker.latest_message["text"]
             service = tracker.latest_message["entities"][0]["value"]
-            print(service, message)
 
-            insert_instruction = service_comments.insert().values(message=message, service=service)
+            sentiment = sample_analyze_sentiment(message)
+            logger.info(f"[{service}] <- {sentiment}: {message}")
+
+            insert_instruction = service_comments.insert().values(message=message, service=service, sentiment=sentiment)
             db_conn.execute(insert_instruction)
 
         dispatcher.utter_message(response="utter_acknowledge_comment")
